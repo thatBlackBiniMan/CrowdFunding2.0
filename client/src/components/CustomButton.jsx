2c17f53da624b6e43c60a782bae7e94a3d649d6a
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-02-26 23:46:27 +0100

    Still working

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({btntype, title, handleclick, styles}) => {
   return (
-    <div>CustomButton</div>
+    <div>
+     <button type={btntype} className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`} 
+     onClick={handleclick}>
+      {title}
+     </button>
+    </div>
   )
 }
 
