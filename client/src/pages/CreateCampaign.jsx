29c3bbbf0ee7ea8862f9988eaa97ef40d70db887
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-02-27 21:23:15 +0100

    Finished CreateCampaign UI

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ethers } from 'ethers';
+import { money } from '../assets';
+import { CustomButton, FormField } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ form, setForm ] = useState({
+    name: '',
+    title: '',
+    description: '',
+    target: '',
+    deadline: '',
+    image: ''
+  })
+
+  const handleFormFieldChange = (e)=>{
+    setForm({...form, [fieldName] : e.target.value})
+  }
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  console.log(form);
+}
+
   return (
-    <div>CreateCampaign</div>
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && 'Loader...'}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px] cursor-pointer">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
+      </div>
+      
+      <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
+        <div className="flex flex-wrap gap-[40px]">
+           <FormField labelName="Your Name *" placeholder="Solomon Omoruyi" inputType="text" value={form.name} handleChange={(e)=>handleFormFieldChange('name', e)}/>
+           <FormField labelName="Campaign Title *" placeholder="Write a title" inputType="text" value={form.title} handleChange={(e)=>handleFormFieldChange('title', e)}/>
+        </div>
+        <FormField labelName="Story *" placeholder="Write your story" isTextArea value={form.description} handleChange={(e)=>handleFormFieldChange('description', e)}/>
+        
+        <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
+        <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
+        <h4 className="font-epilogue font-bold text-[25px] text-gray-300 ml-[20px]">You will get 99.9% 0f raised amount</h4>
+       </div>
+       <div className="flex flex-wrap gap-[40px]">
+           <FormField labelName="Goal *" placeholder="ETH 1.2" inputType="text" value={form.target} handleChange={(e)=>handleFormFieldChange('target', e)}/>
+           <FormField labelName="End Date *" placeholder="End Date" inputType="date" value={form.deadline} handleChange={(e)=>handleFormFieldChange('deadline', e)}/>
+        </div>
+      <FormField labelName="Campaign Image *" placeholder="Place image URL of your campaign" inputType="url" value={form.image} handleChange={(e)=>handleFormFieldChange('image', e)}/>
+        
+      <div className="flex justify-center items-center mt-[40px]">
+          <CustomButton btntype="submit" title="Submit new campaign" styles="bg-[#1dc071]"/>
+      </div>
+    </form>
+  </div>
   )
 }
 
