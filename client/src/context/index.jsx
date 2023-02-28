303616027ed4ba82a2e713c07f04182b00865928
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-02-28 22:47:13 +0100

    Completed Profile page

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -26,12 +26,42 @@ export const StateContextProvider = ({ children }) => {
       console.log("contract call failure", error)
     }
   }
+
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }));
+
+    return parsedCampaings;
+  }
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+    return filteredCampaigns;
+  }
+
+
    return (
     <StateContext.Provider value={{
       address,
       contract,
       connect,
       createCampaign: publishCampaign,
+      getCampaigns,
+      getUserCampaigns
     }}>
       {children}
     </StateContext.Provider>
