657657433fb387b53454d800176cf63cd1dcd1fc
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-02-28 13:29:28 +0100

    having issues with create campaign button

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,44 +1,44 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
+
+import { useStateContext } from '../context';
 import { money } from '../assets';
 import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils';
-import { useStateContext } from '../context';
-import { parseUnits } from 'ethers/lib/utils';
 
 const CreateCampaign = () => {
   const navigate = useNavigate();
-  const [ isLoading, setIsLoading ] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const { createCampaign } = useStateContext();
-  const [ form, setForm ] = useState({
+  const [form, setForm] = useState({
     name: '',
     title: '',
     description: '',
-    target: '',
+    target: '', 
     deadline: '',
     image: ''
-  })
+  });
 
-  const handleFormFieldChange = (e)=>{
-    setForm({...form, [fieldName] : e.target.value})
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value })
   }
-const handleSubmit = async (e)=>{
-  e.preventDefault();
 
-  checkIfImage(form.image, async (exists)=>{
-    if (exists){
-      setIsLoading(true)
-      await createCampaign({...form, target: ethers.utils.parseUnits(form.target, 18)})
-      setIsLoading(false);
-      navigate('/');
-    } else{
-      alert('Provide valid image URL')
-      setForm({...form, image: ''});
-    }
-  })
-  console.log(form);
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    checkIfImage(form.image, async (exists) => {
+      if(exists) {
+        setIsLoading(true)
+        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+        setIsLoading(false);
+        navigate('/');
+      } else {
+        alert('Provide valid image URL')
+        setForm({ ...form, image: '' });
+      }
+    })
+  }
 
   return (
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
@@ -49,11 +49,29 @@ const handleSubmit = async (e)=>{
       
       <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
         <div className="flex flex-wrap gap-[40px]">
-           <FormField labelName="Your Name *" placeholder="Solomon Omoruyi" inputType="text" value={form.name} handleChange={(e)=>handleFormFieldChange('name', e)}/>
-           <FormField labelName="Campaign Title *" placeholder="Write a title" inputType="text" value={form.title} handleChange={(e)=>handleFormFieldChange('title', e)}/>
+        <FormField 
+            labelName="Your Name *"
+            placeholder="Omoruyi Solomon"
+            inputType="text"
+            value={form.name}
+            handleChange={(e) => handleFormFieldChange('name', e)}
+          />
+          <FormField 
+            labelName="Campaign Title *"
+            placeholder="Write a title"
+            inputType="text"
+            value={form.title}
+            handleChange={(e) => handleFormFieldChange('title', e)}
+          />
+
         </div>
-        <FormField labelName="Story *" placeholder="Write your story" isTextArea value={form.description} handleChange={(e)=>handleFormFieldChange('description', e)}/>
-        
+        <FormField 
+            labelName="Story *"
+            placeholder="Write your story"
+            isTextArea
+            value={form.description}
+            handleChange={(e) => handleFormFieldChange('description', e)}
+          /> 
         <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
         <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
         <h4 className="font-epilogue font-bold text-[25px] text-gray-300 ml-[20px]">You will get 99.9% 0f raised amount</h4>
