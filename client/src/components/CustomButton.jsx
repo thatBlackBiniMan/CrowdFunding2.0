3c8a599433b2359cce46f5b757c057f2f158ddc8
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-02-28 07:55:11 +0100

    Connecting with metamask wallet

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const CustomButton = ({btntype, title, handleclick, styles}) => {
+const CustomButton = ({ btnType, title, handleClick, styles }) => {
   return (
-    <div>
-     <button type={btntype} className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`} 
-     onClick={handleclick}>
+    <button
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleClick}
+    >
       {title}
-     </button>
-    </div>
+    </button>
   )
 }
 
